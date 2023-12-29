089dab525abc0ae05eb7fa0ea6984d062e1aeff6
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2023-12-29 12:19:55 +0100

    second commit

---
+++ src/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <div className="main">
+            <header>
+
+                <nav>
+                    <ul>
+                        <li>HOME</li>
+                        <li>NEWS</li>
+                        <li>ABOUT</li>
+                        <li>CONTACT</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default Navbar;
