3b81d4a48a914ddf6c806ebaa6ccdda15321b438
IBRAHIM MUHAMMAD <bibcodes5@gmail.com>
2024-03-20 09:35:07 +0100

    added mission

--- src/info.js
+++ src/info.js
@@ -3,13 +3,11 @@ const Info = () => {
         <div className="news ml-20">
             <div className='w-72 h-44 rounded-2xl vision'>
                 <h2 className='text-center font-bold'>Vision</h2>
-                <h4 className='text-center'>The school have probably enlithened the education
-                    of many student who have now become successful in life.
-                </h4>
+                <h4 className='text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, cum, atque sint libero sit incidunt laborum culpa quidem! Voluptas suscipit nostrum voluptatibus ducimus commodi itaque beatae.</h4>
             </div>
-            <div className="mision">
-                <h2>Mision</h2>
-                <h4>the school only goal is to be able to assist </h4>
+            <div className="w-72 h-44 rounded-2xl mision">
+                <h2 className="text-center font-bold">Mision</h2>
+                <h4 className="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, cum, atque sint libero sit incidunt laborum culpa quidem! Voluptas suscipit nostrum voluptatibus ducimus commodi itaque beatae.</h4>
             </div>
         </div>
     );
